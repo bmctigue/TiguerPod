591cbb4fad9596def1dae327833dce5ceaff8e44
Bruce McTigue <bmctigue@tiguer.com>
2019-02-06 11:24:23 -0700

    Updated the presenter protocol

--- Tiguer/Protocols.swift
+++ Tiguer/Protocols.swift
@@ -42,7 +42,7 @@ public protocol InteractorProtocol: class {
 public protocol PresenterProtocol {
     associatedtype Model
     associatedtype ViewModel
-    func updateViewModels(response: Response<Model>)
+    func updateViewModels(_ response: Response<Model>)
 }
 
 public protocol NetworkSession {
